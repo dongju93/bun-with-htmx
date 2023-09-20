200df9fd91e15ba3e93a9e9248a453bc07433a5d
dong-ju <spdlqj011@gmail.com>
2023-09-21 03:04:26 +0900

    web server and tls connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-Bun.build({
-    entrypoints: ["./src/index.tsx"],
-    outdir: "./build",
-    external: ["react"],
-});
+// Bun.build({
+//     entrypoints: ["./src/index.tsx"],
+//     outdir: "./build",
+//     external: ["react"],
+// });
+
+import { type ServeOptions } from "bun";
+const fileloation = "222";
+
+Bun.serve({
+    async fetch(request: Request) {
+        const url = new URL(request.url);
+        return new Response(url.href);
+    },
+
+    error(error) {
+        console.error(error);
+        return new Response("oops!");
+    },
+
+    // for enable https(cert)
+    tls: {
+        key: Bun.file(`${fileloation}`),
+        cert: Bun.file(`${fileloation}`),
+    },
+} as ServeOptions);
